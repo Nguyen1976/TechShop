cfacfbb8f980864d09d111fa62ec95cb1f2d3142
Nguyen1976 <nguyen2202794@gmail.com>
2024-10-01 22:55:59 +0700

    Update base project

---
+++ fe/ui-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd';
+
+function App() {
+  return (
+    <div>
+        <Button type="primary">Hello world</Button>
+    </div>
+  );
+}
+
+export default App;
