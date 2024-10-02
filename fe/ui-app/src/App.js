f679ed05d9eb646542ba027fe594dddf6dd790b9
Nguyen1976 <nguyen2202794@gmail.com>
2024-10-02 09:02:52 +0700

    set up project redux ant styled componnet

--- fe/ui-app/src/App.js
+++ fe/ui-app/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { Button } from 'antd';
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './redux/slices/counterSlice'
+import styled from 'styled-components';
 
 function App() {
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+  const Button = styled.button({
+    background: 'gray',
+  });
+
   return (
     <div>
-        <Button type="primary">Hello world</Button>
+      <div>
+        <Button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </Button>
+        <span>{count}</span>
+        <Button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </Button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
